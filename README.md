20c88f14d7fee25ecf162435685f2ecabc64e603
Jackie Deng <jackiedeng@proton.me>
2023-12-16 23:41:16 +1100

    Basic self-feedback ChatGPT invocation

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 RPG-style simulacra of interactions between GPTs for general experimentation.
 Inspired by https://doi.org/10.1145/3586183.3606763.
 Built on Godot game engine for graphics.
+
+### Demo
+(As of 2023-12-16)
+![](demo.gif)
+
+### Instructions to Run
+1. Before running, create `api.key` file and insert your OpenAI key
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,10 +1,15 @@
 extends Node2D
 
-const INITIAL_AGENTS_COUNT = 3
+const INITIAL_AGENTS_COUNT = 1
 
 var agents = Array()
 var turn_in_progress = false
 var _temp_turn_in_progress = false
+var api_key = ""
+var api_headers = ["Content-Type: application/json"]
+var api_endpoint = "https://api.openai.com/v1/chat/completions"
+var initial_prompt = "Pretend you are stuck in a dark room. What's your first instinct?"
+var last_response = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,9 +20,11 @@ func _ready():
 		agent_instance.init_position(Vector2i(randi() % 14, randi() % 8 + 1))
 		agents.append(agent_instance)
 		add_child(agent_instance)
-	# Set up API calls
+	# Set up API calls, load key
+	var key_file = FileAccess.open("res://api.key", FileAccess.READ)
+	api_key = key_file.get_as_text()
+	api_headers.append("Authorization: Bearer " + api_key)
 	$HTTPRequest.request_completed.connect(_on_request_completed)
-
 
 func _input(event):
 	# Random Range Cell-based Movement
@@ -25,14 +32,29 @@ func _input(event):
 		for agent in agents:
 			# Movement
 			agent.approach_cell(Vector2i(randi() % 14, randi() % 8 + 1))
-		# Initiate API call (placeholder)
-		$HTTPRequest.request("https://uselessfacts.jsph.pl/api/v2/facts/random")
+		# Initiate API call
+		var prompt = ""
+		if (last_response == ""):
+			prompt = initial_prompt
+		else:
+			prompt = "You just said \'" + last_response + "\'. You succeed. What do you do next?"
+
+		var body = "{\"model\": \"gpt-3.5-turbo\",
+			\"messages\": [{\"role\": \"user\", \"content\": \"" + prompt + "\"}],
+			\"temperature\": 0.7,
+			\"max_tokens\": 70
+			}"
+			# \"max_tokens:\": 100
+		$HTTPRequest.request(api_endpoint, api_headers, HTTPClient.METHOD_POST, body)
 		turn_in_progress = true
 
 
 func _on_request_completed(result, response_code, headers, body):
 		var json = JSON.parse_string(body.get_string_from_utf8())
-		$TextBox/TextOutput.text = json["text"]
+		print(json)
+		if (json.has("choices")):
+			last_response = json["choices"][0]["message"]["content"]
+		$TextBox/TextOutput.text = last_response
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
